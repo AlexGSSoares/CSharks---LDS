4fb30fd5d5c60ad7d200ec7be3ba82404a24d761
unknown <2200169@estudante.uab.pt>
2024-06-02 04:48:23 +0100

    updated Main

---
+++ Main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace SevenZipFrontend {
+    static class Program {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main() {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        var mainForm = new MainForm();
+        var archiveController = new ArchiveController(new ArchiveManager(), mainForm);
+        mainForm.SetController(archiveController);
+        Application.Run(mainForm);
+    }
+}
+}
