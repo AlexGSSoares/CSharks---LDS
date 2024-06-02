9f5df6c13b7b11e96d0187a519e772a7ddb4b7b1
unknown <2200169@estudante.uab.pt>
2024-06-02 04:40:42 +0100

    selection archive/files to foreground

--- temp_main.cs
+++
@@ -1,9 +0,0 @@
-namespace SevenZipFrontend{
-    class Program {
-        static void Main(string[] args) {
-            
-            ArchiveController controller = new ArchiveController();
-            controller.ProcessUserInput();
-        }
-    }
-}
